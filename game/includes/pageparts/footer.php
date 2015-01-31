9b3d825ef0d8f9840333d0aa6ba31d49c0ae7d30
nickdhondt <nick.dhondt38@gmail.com>
2015-01-31 19:21:49 +0100

    Improved footer.php
    Added comments

--- game/includes/pageparts/footer.php
+++ game/includes/pageparts/footer.php
@@ -3,13 +3,15 @@
     <div>
         <?php echo date("d F - H:i:s"); ?> &mdash; Gegenereerd in <?php
 
+        // Displaying the serverdate and the execution time of the script
+        // $page_cal_start is declared in functions.php at the beginning of each page (functions.php in included at the top of each page)
         $page_cal_microseconds = (microtime(true) - $page_cal_start) * 1000;
         echo number_format($page_cal_microseconds, 0, ",", ".");
 
         ?>ms
     </div>
     <div>
-        &copy; Goldenratio
+        &copy; Goldenratio Interactive
     </div>
 </footer>
 </div>
